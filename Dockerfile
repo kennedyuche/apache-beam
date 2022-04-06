249705707b64d585667624bab9f615594e6aab4e
KennedyUC <chukwukakennedyuche@gmail.com>
2022-04-06 11:18:27 -0700

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM python:3.8
+
+RUN python3 -m venv /opt/venv
+
+USER root
+
+WORKDIR /app
+
+RUN apt-get install python3-pip
+
+# Install dependencies
+COPY requirements.txt /app/
+RUN /opt/venv/bin/activate && pip install -r requirements.txt
+
+# Run job
+COPY beam-flink-pipeline.py  /app
+CMD /opt/venv/bin/activate && exec python beam-flink-pipeline.py
+
+ENTRYPOINT [ "/opt/entrypoint.sh" ]
---
+++ beam-flink-pipeline.py
@@ -0,0 +1,27 @@
+import apache_beam as beam
+from apache_beam.io import ReadFromText
+from apache_beam.io import WriteToText
+from apache_beam.options.pipeline_options import PipelineOptions
+import apache_beam.transforms.window as window
+import re
+
+input_file = '.\words.txt'
+output_path = '.\cleaned.txt'
+
+def run():
+    options = PipelineOptions([
+        "--runner=FlinkRunner",
+        "--flink_version=1.13",
+        "--flink_master=localhost:8081"
+    ])
+
+    with beam.Pipeline(options=options) as pipeline:
+        (pipeline
+            | 'Read Text file' >> beam.io.ReadFromText(input_file)
+            | 'Split words'    >> beam.FlatMap(lambda words: words.split(' '))
+            | 'Clean words'    >> beam.Map(lambda word: re.sub('[^A-Za-z]+','', word))
+            | 'Write to file'  >> beam.io.WriteToText(output_path)
+        )
+
+if __name__ == "__main__":
+    run()
